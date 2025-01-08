7949da3d7b039a244cfef89041dc72c0ea0a5bef
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2025-01-08 10:46:34 +0100

    add authentication to proxy

--- src/proxy/merger.py
+++ src/proxy/merger.py
@@ -3,10 +3,11 @@ from ldaptor.protocols.pureldap import LDAPBindRequest
 from twisted.internet import protocol, defer, reactor
 from ldaptor.config import LDAPConfig
 from ldaptor.protocols.ldap.merger import MergedLDAPServer
-from proxydatabase import ServerEntry
+from proxydatabase import LdapProxyDatabase, ServerEntry, UserEntry
 
 class ProxyMerger(MergedLDAPServer):
-    def __init__(self, configs: [ServerEntry]):
+    def __init__(self, database: LdapProxyDatabase):
+        configs = database.get_servers()
         c = [self._ldap_config_from_db_entry(i) for i in configs]
         self.credentials = [i[1] for i in c]
         super().__init__([i[0] for i in c], [c.tls for c in configs])
@@ -14,7 +15,11 @@ class ProxyMerger(MergedLDAPServer):
     def handle_LDAPBindRequest(self, request, controls, reply):
         self._whenConnected(self._handle_LDAPBindRequest, request, controls, reply)
     def _handle_LDAPBindRequest(self, request: LDAPBindRequest, controls, reply):
-        # TODO check if credentials are valid
+        # authenticate user
+        auth_user = self.authenticate_user(request.dn, request.auth)
+        if auth_user is None:
+            raise Exception("User not authorized")
+            
         for client, creds in zip(self.clients, self.credentials):
             ldap_bind_request = LDAPBindRequest(version=3, dn=creds[0], auth=creds[1])
             d = client.send_multiResponse(ldap_bind_request, self._gotResponse, reply)
@@ -29,17 +34,27 @@ class ProxyMerger(MergedLDAPServer):
     def _ldap_config_from_db_entry(self, config: ServerEntry):
         return (
             LDAPConfig(serviceLocationOverrides={"": (config.ip, config.port)}),
-            (config.bind_dn, config.bind_password))
+            (config.bind_dn, config.bind_password)
+        )
+    
+    # authenticate a user. Return None if not authorized
+    def authenticate_user(self, dn, auth):
+        users = self.database.get_users()
+        for u in users:
+            if u["user"] == dn and u["password"] == auth:
+                return UserEntry(dn, auth, u["is_admin"])
+        return None
 
 if __name__ == '__main__':
     factory = protocol.ServerFactory()
 
     def buildProtocol():
-        configs = [
-            ServerEntry(ip="192.168.1.2", port=389, bind_dn="", bind_password="dc", base_dn=""),
-            ServerEntry(ip="192.168.1.189", port=389, bind_dn="", bind_password="dc-temp", base_dn="")
-        ]
-        return ProxyMerger(configs)
+        db = LdapProxyDatabase("127.0.0.1", "27017")
+        # configs = [
+        #     ServerEntry(ip="192.168.1.2", port=389, bind_dn="", bind_password="dc", base_dn=""),
+        #     ServerEntry(ip="192.168.1.189", port=389, bind_dn="", bind_password="dc-temp", base_dn="")
+        # ]
+        return ProxyMerger(db)
 
     factory.protocol = buildProtocol
     reactor.listenTCP(3389, factory)
