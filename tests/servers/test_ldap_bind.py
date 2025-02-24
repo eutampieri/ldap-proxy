f6e0ca3494b0420dc437c1abd377e51a45c28429
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2025-02-24 11:17:57 +0100

    add testing base for ldap

---
+++ tests/servers/test_ldap_bind.py
@@ -0,0 +1,39 @@
+from twisted.internet import defer
+from twisted.internet.task import react
+from twisted.internet.endpoints import clientFromString, connectProtocol
+from ldaptor import ldapfilter
+from ldaptor.protocols.ldap import ldapsyntax
+from ldaptor.protocols.ldap.ldapconnector import connectToLDAPEndpoint
+from ldaptor.protocols.ldap.ldapclient import LDAPClient
+from ldaptor.protocols.ldap.distinguishedname import DistinguishedName
+
+def search(reactor, endpointStr, base_dn):
+    d = connectToLDAPEndpoint(reactor, endpointStr, LDAPClient)
+
+    def _doSearch(proto):
+        searchFilter = ldapfilter.parseFilter('(gn=j*)')
+        baseEntry = ldapsyntax.LDAPEntry(client=proto, dn=base_dn)
+        d = baseEntry.search(filterObject=searchFilter)
+        return d
+
+    d.addCallback(_doSearch)
+    return d
+
+def main(reactor):
+    import sys
+    from twisted.python import log
+    log.startLogging(sys.stderr, setStdout=0)
+    dn =  DistinguishedName('dc=example,dc=org')
+    d = search(reactor, "tcp:localhost:10389", dn)
+
+    def _show(results):
+        for item in results:
+            print(item)
+
+    d.addCallback(_show)
+    d.addErrback(defer.logError)
+    d.addBoth(lambda _: reactor.stop())
+    return d
+
+if __name__ == '__main__':
+    react(main)
