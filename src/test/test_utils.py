3bb54715925ef7487cf54f86f0c97468739e5749
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2025-02-26 11:28:16 +0100

    refactor of test and mocks

--- src/test/test_utils.py
+++ src/test/test_utils.py
@@ -1,60 +1,7 @@
-from twisted.internet import reactor, defer
+from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 from twisted.internet.protocol import Factory
-from twisted.internet.endpoints import clientFromString
-from ldaptor.protocols import pureldap
-from ldaptor.protocols.ldap.ldapserver import LDAPServer
-from ldaptor.protocols.ldap import ldapsyntax
-from ldaptor.protocols.ldap.ldapconnector import connectToLDAPEndpoint
-from ldaptor.protocols.ldap.ldapclient import LDAPClient
-from ldaptor.protocols.ldap.distinguishedname import DistinguishedName
-
-### LDAP Client ###
-
-class MockLDAPClient():
-    """A base mock for a LDAP client."""
-    def run(self, endpoint) -> Deferred:
-        """Executes the query of this client."""
-        pass
-
-class BindingClient(MockLDAPClient):
-    """A mock LDAP client that makes a bind request."""
-    def __init__(self, dn: str, password: str):
-        self.dn = dn
-        self.password = password
-
-    def bind(self, endpoint, dn, password):
-        d = connectToLDAPEndpoint(reactor, endpoint, LDAPClient)
-
-        def _doBind(proto):
-            baseEntry = ldapsyntax.LDAPEntry(client=proto, dn=DistinguishedName(dn))
-            x = baseEntry.bind(password=password)
-            return x
-        
-        d.addCallback(_doBind)
-        return d
-
-    def run(self, endpoint) -> Deferred:
-        d = self.bind(endpoint, self.dn, self.password)
-        d.addErrback(defer.logError)
-        return d
-
-### LDAP Server ###
-
-class MockLDAPServer(LDAPServer):
-    """A base mock for a LDAP server. Needs to be extended with the wanted capabilities."""
-
-class AcceptBind(MockLDAPServer):
-    """A mock LDAP server that accepts all bind requests."""
-    def handle_LDAPBindRequest(self, request, controls, reply):
-        # Accept bind request
-        return reply(pureldap.LDAPBindResponse(resultCode=0))
-    
-class RejectBind(MockLDAPServer):
-    """A mock LDAP server that rejects all bind requests."""
-    def handle_LDAPBindRequest(self, request, controls, reply):
-        # Reject bind request (invalid credentials)
-        return reply(pureldap.LDAPBindResponse(resultCode=49))
+from mocks import MockLDAPClient, MockLDAPServer
     
 class TestUtils:
     """Utility module for testing multip[le servers and clients together."""
@@ -112,6 +59,8 @@ class TestUtils:
 
 if __name__ == "__main__":
     # Testing the testing framework
+    from mocks import AcceptBind, RejectBind, BindingClient
+
     test = TestUtils()
     test.addServer(port=3890, server=AcceptBind)
     test.addServer(port=3891, server=RejectBind)
