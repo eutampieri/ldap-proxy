d425317377a8e73fce33a17e85a135b058239fad
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2025-08-07 19:07:17 +0200

    Dump example python object as JSON

---
+++ tests/schemas/test_python.py
@@ -0,0 +1,15 @@
+import json
+import sys
+sys.path.append("src/proxy")
+
+from proxydatabase import *
+
+data = [
+    ("server", ServerEntry("127.0.0.1", "389", "", "", "")),
+    ("user", UserEntry("user", "pass", True)),
+    ("client", ClientEntry("dn", "pass")),
+]
+
+for t, e in data:
+    with open(f"{t}_python.json", "w") as f:
+        json.dump(e.to_object(), f)
